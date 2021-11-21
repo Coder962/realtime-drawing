99cf6d5a24ce1a71c215ffa99c41c01bde77a90f
Coder962 <76476530+Coder962@users.noreply.github.com>
2021-11-21 12:39:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -13,8 +18,19 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log("results");
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY =" + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " +" rightWristX = "+ rightWristX + " difference = "+ difference);
     }
 }
 function draw() {
-    background('##808080');
+    background('#808080');
+    fill('#ff007f');
+    stroke('#1b03a3');
+    square(noseX,noseY,difference);
 }
